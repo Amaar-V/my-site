7f118d1bb561ee0e7f7610844fd14eac3926ea84
AmaarValliani <avallian@ucsd.edu>
2023-10-31 23:35:58 -0700

    updated text color and images

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/img-bg.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Amaar Valliani</a></h1>
               <span className="email"><i className="icon-mail"></i> amaarvalliani@gmail.com</span>
               <span className="email"><i className="icon-mail"></i> avallian@ucsd.edu</span>
