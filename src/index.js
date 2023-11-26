61504151e3a6bb2c7edd624f3113a99bd5831f61
Amaar <avallian@ucsd.edu>
2023-11-26 11:21:38 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 
-ReactDOM.render((
-  <HashRouter>
-    <App /> 
-  </HashRouter>
-  ), document.getElementById('root')
-);
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.render(<RouterProvider router={router} />, document.getElementById('root'));
+
 
 
 // If you want to start measuring performance in your app, pass a function
