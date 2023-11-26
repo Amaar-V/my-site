57322e6989f9b48eb46397a42cc3c26f7cd6eb86
Amaar <avallian@ucsd.edu>
2023-11-26 11:45:20 -0800

    change homepage

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
 
 
 ReactDOM.render((
